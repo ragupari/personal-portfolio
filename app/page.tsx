41db83eece8a0741ce57302f8faa5093b5fcf76f
Parishith Ragumar <ragupari07@gmail.com>
2026-02-10 23:40:48 +0530

    Add publications section to the page

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,7 @@ import {
   SOCIAL_LINKS,
   VOLUNTEER_EXPERIENCE,
   CERTIFICATIONS,
+  PUBLICATIONS,
 } from './data'
 
 const VARIANTS_CONTAINER = {
@@ -249,6 +250,43 @@ export default function Personal() {
           </div>
           <p className="text-zinc-600 dark:text-zinc-400">
             {school.start} - {school.end}
+          </p>
+            </div>
+          </div>
+        </div>
+          ))}
+        </div>
+      </motion.section>
+      <motion.section
+        variants={VARIANTS_SECTION}
+        transition={TRANSITION_SECTION}
+      >
+        <h3 className="mb-5 text-lg font-medium">Publications</h3>
+        <div className="flex flex-col space-y-2">
+          {PUBLICATIONS.map((pub) => (
+        <div
+          className="relative overflow-hidden rounded-2xl bg-zinc-300/30 p-[1px] dark:bg-zinc-600/30"
+          key={pub.id}
+        >
+          <Spotlight
+            className="from-zinc-900 via-zinc-800 to-zinc-700 blur-2xl dark:from-zinc-100 dark:via-zinc-200 dark:to-zinc-50"
+            size={64}
+          />
+          <div className="relative h-full w-full rounded-[15px] bg-white p-4 dark:bg-zinc-950">
+            <div className="relative flex w-full flex-row justify-between">
+          <div>
+            <h4 className="font-normal dark:text-zinc-100">
+              {pub.title}
+            </h4>
+            <p className="text-zinc-500 dark:text-zinc-400">
+              {pub.organization}
+            </p>
+            <p className="text-zinc-500 dark:text-zinc-400">
+              {pub.authors}
+            </p>
+          </div>
+          <p className="text-zinc-600 dark:text-zinc-400">
+            {pub.year}
           </p>
             </div>
           </div>
